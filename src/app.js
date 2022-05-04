160cfb7b3e52cb57e488a1a50c2a08dc38c4f3b4
Alb3rnaru <moreno.alberto907@gmail.com>
2022-05-04 22:37:23 +0000

    :sparkles: Subiendo excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car", "the fork"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function randomNumber() {
+    return Math.floor(Math.random() * (3 - 0));
+  }
+  document.getElementById("excuse").innerHTML =
+    who[randomNumber()] +
+    " " +
+    action[randomNumber()] +
+    " " +
+    what[randomNumber()] +
+    " " +
+    when[randomNumber()];
   console.log("Hello Rigo from the console!");
 };
